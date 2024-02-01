c02fa23de7426cc9dfdaf2d75d8a4e23547c3586
Georg Makowski <gm@desider.at>
2024-02-01 19:18:47 +0100

    Update Go versions and testing logic in Github actions
    The .github/workflows/test.yaml file has been updated to include Go 1.21.x in testing. Additionally, the superscript_test.go and subscript_test.go testing logic has been refactored.

--- subscript_test.go
+++ subscript_test.go
@@ -5,17 +5,13 @@ import (
 	"testing"
 
 	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/renderer/html"
 	"github.com/yuin/goldmark/testutil"
 )
 
 func TestSubscript(t *testing.T) {
 	markdown := goldmark.New(
-		goldmark.WithRendererOptions(
-			html.WithUnsafe(),
-		),
 		goldmark.WithExtensions(
-			Subscript,
+			Subscript, Superscript,
 			extension.Strikethrough,
 		),
 	)
--- superscript_test.go
+++ superscript_test.go
@@ -4,17 +4,13 @@ import (
 	"testing"
 
 	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/renderer/html"
 	"github.com/yuin/goldmark/testutil"
 )
 
 func TestSuperscript(t *testing.T) {
 	markdown := goldmark.New(
-		goldmark.WithRendererOptions(
-			html.WithUnsafe(),
-		),
 		goldmark.WithExtensions(
-			Superscript,
+			Superscript, Subscript,
 		),
 	)
 	testutil.DoTestCaseFile(markdown, "_test/superscript.txt", t, testutil.ParseCliCaseArg()...)
